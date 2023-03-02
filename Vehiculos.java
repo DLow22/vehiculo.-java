a298dab15786fe60545b7bd56d0fcc5bccc72f3d
DLow22 <124116674+DLow22@users.noreply.github.com>
2023-03-02 00:24:14 -0600

    Add files via upload

---
+++ Vehiculos.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class Vehiculos {
+
+    private String placa, marca, modelo;
+    private int kilometraje;
+
+    Vehiculos( String placa, String marca, String modelo, int kilometraje){
+        setPlaca(placa);
+        setMarca(marca);
+        setModelo(modelo);
+        setKilometraje(kilometraje);
+
+
+    }
+    /*Metodos Modificadores */
+    public void setPlaca(String n){ placa = n; }
+    public void setMarca(String a){ marca = a; }
+    public void setModelo(String c){ modelo = c; }
+    public void setKilometraje(int e){ kilometraje = e; }
+    /*Metodos Accesores */
+    public String getplaca(){return placa;}
+    public String getmarca(){return marca;}
+    public String getmodelo(){return modelo;}
+    public int getkilometraje(){return kilometraje;}
+
+
+    public void mostrarvehiculos(){
+        System.out.println("\nplaca: " +getplaca()+"\nmarca: " +getmarca()+ "\nmodelo: " +getmodelo()+
+                    "\nKilometraje: " +getkilometraje() );
+
+
+    }
+    public class TestVehiculos {
+           public static void main(String[ ] args) {
+        try {Scanner entrada = new Scanner (System.in)} {
+            String placa, marca, modelo;
+                int kilometraje;
+                System.out.println("Favor de ingresar placa: ");
+
+                placa = entrada.nextLine();
+                System.out.println("Favor de ingrasar maraca: ");
+                marca = entrada.nextLine();
+                System.out.println("Favor de ingresar modeelo: ");
+                modelo = entrada.nextLine();
+                System.out.println("Favor de ingresar Kilometraje: ");
+                kilometraje = entrada.nextInt();
+                Vehiculos e;
+                e = new Vehiculos(placa, marca, modelo, kilometraje);
+                e.mostrarvehiculos();
+
+        }
+
+                
+             
+        
+          }
+          public String getPlaca(){
+            return placa;
+          }
+          public String getMarca(){
+            return marca;
+          }
+          public String getModelo(){
+            return modelo;
+          }
+          public int getKilometraje(){
+            return kilometraje;
+          }
+ }
+
+ }
+    
+    
+
